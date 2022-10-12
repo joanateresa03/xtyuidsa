48b5be6ae3db66a99fed3fb4140e039a5d15907d
joanateresa03 <115594017+joanateresa03@users.noreply.github.com>
2022-10-12 02:51:17 +0100

    Add files via upload

---
+++ admin/version.php
@@ -0,0 +1,8 @@
+<h2>
+    <a href="https://yellowweb.top/donate" target="_blank">
+        Version: 27.08.2022
+        <br/>
+        PHP Version: <?=phpversion()?>
+        <br/> Please, donate!
+    </a>
+</h2>
